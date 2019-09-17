faddc552fe4563a4d8a1b17c1f85b0d39de36721
Tony Tom <ttom@perleybrook.com>
2019-09-17 12:12:47 +0530

    products display design

---
+++ user/products.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>
+    User Sign up
+    </title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Online Watch Store</a>
+    </nav>
+    <div class="container">
+    <h3 class="h3 mt-5 mb-5">Purchase your favourite watch</h3>
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid6">
+                <div class="product-image6">
+                    <a href="#">
+                        <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo10/images/img-1.jpg">
+                    </a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Men's Shirt</a></h3>
+                    <div class="price">$11.00</div>
+                    <button class="btn btn-warning">Add to cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+</body>
+</html>
