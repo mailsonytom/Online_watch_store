7b8b3b9a65466ed29660e7f046f0d52f5289646e
Tony Tom <macbookpro@macbooks-MacBook-Pro.local>
2019-12-14 15:12:46 +0530

    card update

--- user/cart.php
+++ user/cart.php
@@ -9,6 +9,7 @@ if (!isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
     $sql = "SELECT * FROM products INNER JOIN cart on cart.product_id = products.id WHERE user_id='$user_id'";
     $result = mysqli_query($conn, $sql);
+    $num_rows = mysqli_num_rows($result);
     while ($row = mysqli_fetch_assoc($result)) {
         $data[] = $row;
     }
@@ -82,31 +83,33 @@ if (!isset($_SESSION['user_id'])) {
             </div>
             <div class="row">
                 <?php $total = 0;
-                    foreach ($data as $a) { ?>
-                    <div class="alert alert-success col-md-12" role="alert">
-                        <div class="row">
-                            <div class="col-md-2">
-                                <img src="../images/<?php echo $a['image'] ?>" alt="" class="img-fluid col-md-8" />
-                            </div>
-                            <div class="col-md-3">
-                                <h4 class="alert-heading"><?php echo $a['name']; ?></h4>
-                                <p class="text-dark"><?php echo $a['description']; ?></p>
-                                <span class="badge badge-primary"><?php echo $a['brand']; ?></span>
-                                <span class="badge badge-secondary"><?php echo $a['code']; ?></span>
-                                <span class="badge badge-success"><?php echo $a['category']; ?></span>
-                                <span class="badge badge-danger"><?php echo $a['gender']; ?></span>
-                                <span class="badge badge-warning"><?php echo $a['type']; ?></span>
-                                <p class="mb-0">Price in INR: ₹<?php echo $a['price']; ?></p>
-                            </div>
-                            <div class="col-md-1 offset-4">
-                                <span class="badge badge-success">Count: <?php echo $a['count']; ?></span>
-                                <span class="badge badge-warning">Price: ₹<?php echo $a['count'] * $a['price'];
-                                                                                    $total += $a['count'] * $a['price']; ?></span>
-                                <a href="removefromcart.php?id=<?php echo $a['id']; ?>"><span class="mt-4 badge badge-danger">Remove from cart</span></a>
+                    if ($num_rows > 0) {
+                        foreach ($data as $a) { ?>
+                        <div class="alert alert-success col-md-12" role="alert">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <img src="../images/<?php echo $a['image'] ?>" alt="" class="img-fluid col-md-8" />
+                                </div>
+                                <div class="col-md-3">
+                                    <h4 class="alert-heading"><?php echo $a['name']; ?></h4>
+                                    <p class="text-dark"><?php echo $a['description']; ?></p>
+                                    <span class="badge badge-primary"><?php echo $a['brand']; ?></span>
+                                    <span class="badge badge-secondary"><?php echo $a['code']; ?></span>
+                                    <span class="badge badge-success"><?php echo $a['category']; ?></span>
+                                    <span class="badge badge-danger"><?php echo $a['gender']; ?></span>
+                                    <span class="badge badge-warning"><?php echo $a['type']; ?></span>
+                                    <p class="mb-0">Price in INR: ₹<?php echo $a['price']; ?></p>
+                                </div>
+                                <div class="col-md-1 offset-4">
+                                    <span class="badge badge-success">Count: <?php echo $a['count']; ?></span>
+                                    <span class="badge badge-warning">Price: ₹<?php echo $a['count'] * $a['price'];
+                                                                                            $total += $a['count'] * $a['price']; ?></span>
+                                    <a href="removefromcart.php?id=<?php echo $a['id']; ?>"><span class="mt-4 badge badge-danger">Remove from cart</span></a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                <?php } ?>
+                <?php }
+                    } ?>
             </div>
             <hr>
             <div class="row mt-5 mb-3">
